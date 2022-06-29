4a6e87486286e55c78316ba85ec9923a5b3b16e9
Britone Mwasaru <bmwasaru@gmail.com>
2022-06-30 02:11:56 +0300

    Local run

--- scripts/get_news.py
+++ scripts/get_news.py
@@ -1,26 +1,26 @@
 from utils import VoaSwahili
 
 
-# news_links = {
-#     'headlines': "https://www.voaswahili.com/",
-#     "kenya": "https://www.voaswahili.com/kenya",
-#     "tanzania": "https://www.voaswahili.com/tanzania",
-#     "kongo": "https://www.voaswahili.com/jamhuri-ya-kidemokrasia-ya-kongo",
-#     "rwanda": "https://www.voaswahili.com/rwanda",
-#     "uganda": "https://www.voaswahili.com/uganda",
-#     "burundi": "https://www.voaswahili.com/burundi",
-#     "afrika": "https://www.voaswahili.com/afrika",
-#     "marekani": "https://www.voaswahili.com/marekani",
-#     "dunia": "https://www.voaswahili.com/dunia",
-# }
+news_links = {
+    'headlines': "https://www.voaswahili.com/",
+    "kenya": "https://www.voaswahili.com/kenya",
+    "tanzania": "https://www.voaswahili.com/tanzania",
+    "kongo": "https://www.voaswahili.com/jamhuri-ya-kidemokrasia-ya-kongo",
+    "rwanda": "https://www.voaswahili.com/rwanda",
+    "uganda": "https://www.voaswahili.com/uganda",
+    "burundi": "https://www.voaswahili.com/burundi",
+    "afrika": "https://www.voaswahili.com/afrika",
+    "marekani": "https://www.voaswahili.com/marekani",
+    "dunia": "https://www.voaswahili.com/dunia",
+}
 
 
-# for k, v in news_links.items():
-#     news = VoaSwahili(v, k)
-#     news.get_page_headlines()
-#     news.get_page_content(content_class="content-floated-wrap fb-quotable")
+for k, v in news_links.items():
+    news = VoaSwahili(v, k)
+    news.get_page_headlines()
+    news.get_page_content(content_class="content-floated-wrap fb-quotable")
 
 
-special_news = VoaSwahili("https://www.voaswahili.com/makala-maalum", "makala-maalum")
-special_news.get_page_headlines()
-special_news.get_page_content(content_class="container container--featured m-t-md")
+# special_news = VoaSwahili("https://www.voaswahili.com/makala-maalum", "makala-maalum")
+# special_news.get_page_headlines()
+# special_news.get_page_content(content_class="container container--featured m-t-md")
